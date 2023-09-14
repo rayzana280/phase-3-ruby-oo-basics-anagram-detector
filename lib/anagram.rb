e7ae6fb7d47e89ee4e4a102a9e3928900662ffec
Ray Zana <rayzana280@gmail.com>
2023-09-14 11:50:33 -0400

    commit tree a33552a2608822a5593d989631f379ed3de9e522

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+#get class with initiazlie called anagram 
+#have a method called match which would iterate through the array 
+# find the 
+require "pry"
+class Anagram 
+    attr_accessor :word 
+    def initialize(word)
+        @word = word 
+    end 
+
+    def match(array)
+        array_match = array.filter do |element|
+            @element = element 
+            new_element = @element.chars.sort
+            new_word = @word.chars.sort 
+            if new_element == new_word
+                @element 
+            end 
+        end 
+        #binding.pry
+    end 
+end 
+
